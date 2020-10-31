f15b628ed8cfff0196c9b5e10677b76ca86c6c4e
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-31 15:23:58 -0700

    updated order of readme sections and added some prompt text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // function to generate markdown for README
 function generateMarkdown(response) {
   return `# ${response.title}
+  
+![License](https://img.shields.io/static/v1?label=license&message=${response.license}&color=brightgreen)
 
 ## Description
 ${response.description}
@@ -10,28 +12,37 @@ ${response.description}
 1. [Description](#Description)
 1. [Usage](#Usage)
 1. [Installation](#Installation)
-1. [License](#License)
-1. [Test](#Test)
 1. [Contribution](#Contribution)
+1. [Test](#Test)
+1. [License](#License)
 1. [Questions](#Questions)
 
 ## Usage
 ${response.usage}
 
 ## Installation
+In order to use this project, please use the following command:
+
 ${response.installation}
 
 ## Contribution
+If you are interested in contributing to my project, please note:
+
 ${response.contribution}
 
 ## Test
+In order to test this project, please use the following command:
+
 ${response.test}
 
 ## License
 ${response.license}
 
 ## Questions
-GitHub: @${response.username}
+If you have any questions about my project, I can be reached via:
+
+GitHub: ${response.username}
+
 Email: ${response.email}
 `;
 }
