3525082a01269e74d6591cb8092675bf120fca05
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-30 17:15:09 -0700

    added markdown code to generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `# ${response.title}
 
+## Description
+${response.description}
+
+## Table of Contents
+
+1. [Description](#Description)
+1. [Usage](#Usage)
+1. [Installation](#Installation)
+1. [License](#License)
+1. [Test](#Test)
+1. [Contribution](#Contribution)
+1. [Questions](#Questions)
+
+## Usage
+${response.usage}
+
+## Installation
+${response.installation}
+
+## Contribution
+${response.contribution}
+
+## Test
+${response.test}
+
+## License
+${response.license}
+
+## Questions
+GitHub: @${response.username}
+Email: ${response.email}
 `;
 }
 
